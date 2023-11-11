768abdad7ad776d004f3875a3fa97fad2c1c698b
i3kae <snoger1234@gmail.com>
2023-11-12 04:38:04 +0900

    feat: 세분화 메뉴 필드, 메뉴 계산 함수, 총구매 금액 계산 메소드 구현 1. 기존의 총구매 금액(totalAmount)를 세분화해서 필드로 작성 2. 세분화 메뉴별 계산 함수 구현 3. 세분화 메뉴별 계산 함수를 통해 총구매 금액 계산 메소드 구현

--- src/main/java/christmas/Customer.java
+++ src/main/java/christmas/Customer.java
@@ -1,14 +1,66 @@
 package christmas;
 
 import java.util.EnumMap;
+import java.util.List;
 import java.util.Map;
 
 public class Customer{
     private Integer visitDate;
-    private Map<FoodMenu, Integer> purchaseMenu = new EnumMap<>(FoodMenu.class);
+    private Map<FoodMenu.Appetizer, Integer> appetizerMenu;
 
-    public Customer(Integer visitDate, Map<FoodMenu, Integer> purchaseMenu){
+    private Map<FoodMenu.Main, Integer> mainMenu;
+
+    private Map<FoodMenu.Dessert, Integer> dessertMenu;
+
+    private Map<FoodMenu.Drink, Integer> drinkMenu;
+
+    public Customer(Integer visitDate,
+                    Map<FoodMenu.Appetizer, Integer> appetizerMenu,
+                    Map<FoodMenu.Main, Integer> mainMenu,
+                    Map<FoodMenu.Dessert, Integer> dessertMenu,
+                    Map<FoodMenu.Drink, Integer> drinkMenu){
         this.visitDate = visitDate;
-        this.purchaseMenu = purchaseMenu;
+        this.appetizerMenu = appetizerMenu;
+        this.mainMenu = mainMenu;
+        this.dessertMenu = dessertMenu;
+        this.drinkMenu = drinkMenu;
+    }
+
+    public Integer calcPurchaseAmount(){
+        Integer calcResult = 0;
+        calcResult += calcAppetizerAmount();
+        calcResult += calcMainAmount();
+        calcResult += calcDesserAmount();
+        calcResult += calcDrinkAmount();
+        return calcResult;
+    }
+
+    private Integer calcAppetizerAmount(){
+        Integer calcResult = 0;
+        for (FoodMenu.Appetizer menuName : appetizerMenu.keySet()){
+            calcResult += appetizerMenu.get(menuName) * menuName.getAmount();
+        }
+        return calcResult;
+    }
+    private Integer calcMainAmount(){
+        Integer calcResult = 0;
+        for (FoodMenu.Main menuName : mainMenu.keySet()){
+            calcResult += mainMenu.get(menuName) * menuName.getAmount();
+        }
+        return calcResult;
+    }
+    private Integer calcDesserAmount(){
+        Integer calcResult = 0;
+        for (FoodMenu.Dessert menuName : dessertMenu.keySet()){
+            calcResult += dessertMenu.get(menuName) * menuName.getAmount();
+        }
+        return calcResult;
+    }
+    private Integer calcDrinkAmount(){
+        Integer calcResult = 0;
+        for (FoodMenu.Drink menuName : drinkMenu.keySet()){
+            calcResult += drinkMenu.get(menuName) * menuName.getAmount();
+        }
+        return calcResult;
     }
 }
