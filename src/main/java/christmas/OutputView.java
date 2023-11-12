8274a785f3932dac688aa725fdbb63c0e5b1d10b
i3kae <snoger1234@gmail.com>
2023-11-12 17:50:55 +0900

    feat: 할인 전 총주문 금액 출력 메소드 구현 1. 할인 전 총주문 금액 관련 상수 추가 2. printTotalPurchaseAmount를 통해 인자로 받은 가격 출력

--- src/main/java/christmas/OutputView.java
+++ src/main/java/christmas/OutputView.java
@@ -1,9 +1,14 @@
 package christmas;
 
+import java.text.DecimalFormat;
+
 public class OutputView {
-    public void printMenu() {
-        System.out.println("<주문 메뉴>");
-        // ...
+    private static final String TOTAL_PURCHASE_AMOUNT="<할인 전 총주문 금액>";
+    private static final String AMOUNT_MESSAGE="원";
+    private static final String THREE_NUMBER_FORMAT="###,###";
+    private static final DecimalFormat decFormat = new DecimalFormat(THREE_NUMBER_FORMAT);
+    public void printTotalPurchaseAmount(Integer amount){
+        System.out.println(TOTAL_PURCHASE_AMOUNT);
+        System.out.println(decFormat.format(amount) + AMOUNT_MESSAGE);
     }
-    // ...
 }
