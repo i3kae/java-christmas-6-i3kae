94bd1f366a8919cefa02d837f0b647820017a068
i3kae <snoger1234@gmail.com>
2023-11-13 14:04:45 +0900

    feat: 이벤트 미리보기 메시지 추가

--- src/main/java/christmas/Messages.java
+++ src/main/java/christmas/Messages.java
@@ -4,7 +4,8 @@ public class Messages {
     public enum Main{
         WELCOME_MESSAGE("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."),
         VISIT_MESSAGE("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"),
-        ORDER_MESSAGE("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        ORDER_MESSAGE("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"),
+        EVENT_BENEFIT_PREVIEW("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
         private final String message;
         Main(String message){
             this.message = message;
