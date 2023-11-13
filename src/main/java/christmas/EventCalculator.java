de17b5c4cdca651306b9e00ae37bb47b924345e0
i3kae <snoger1234@gmail.com>
2023-11-13 12:10:08 +0900

    refactor: 할인 혜택 계산 함수로 분리, 증정 이벤트 여부 계산 기능 추가 1. 할인 혜택(평일, 주말, 특별, 크리스마스 디데이)을 별도의 함수로 분리 2. 총구매 금액에 따라 증정 이벤트 여부 계산 3. 구매 금액이 증정 이벤트의 조건에 맞는지 확인하는 isPresentEvent 함수 구현

--- src/main/java/christmas/EventCalculator.java
+++ src/main/java/christmas/EventCalculator.java
@@ -12,6 +12,7 @@ public class EventCalculator {
     private static final int CHRISTMAS_DEFAULT_DISCOUNT = 1000;
     private static final int CHRISTMAS_ADDITIONAL_DISCOUNT = 100;
     private static final int FIRST_DAY = 1;
+    private static final int PRESENT_EVENT_AMOUNT = 120000;
 
     public enum WeekList{
         FRIDAY, SATURDAY, SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY;
@@ -23,7 +24,14 @@ public class EventCalculator {
         STAR, TREE, SANTA;
     }
     private static final int DISCOUNT_WEEK = 2023;
-    public List<EventType> calcEventList(Integer visitDate){
+    public List<EventType> calcEventList(Integer visitDate, Integer purchaseAmount){
+        List<EventType> appliedEvent = calcSaleEvent(visitDate);
+        if (isPresentEvent(purchaseAmount)){
+            appliedEvent.add(EventType.PRESENT);
+        }
+        return appliedEvent;
+    }
+    public List<EventType> calcSaleEvent(Integer visitDate){
         List<EventType> appliedEvent = new ArrayList<>();
         if (isChristmasDDay(visitDate)){
             appliedEvent.add(EventType.CHRISTMAS);
@@ -83,6 +91,12 @@ public class EventCalculator {
     }
     public boolean isChristmasDDay(Integer date){
         if (FIRST_DAY <= date && date <= CHRISTMAS){
+            return true;
+        }
+        return false;
+    }
+    public boolean isPresentEvent(Integer purchaseAmount){
+        if (PRESENT_EVENT_AMOUNT <= purchaseAmount){
             return true;
         }
         return false;
