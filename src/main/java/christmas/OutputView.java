9cbae4972e550271c344c3228d38914fe050c484
i3kae <snoger1234@gmail.com>
2023-11-13 14:26:27 +0900

    feat: 증정 메뉴 출력 함수 구현 1. 이벤트 리스트에 증정 이벤트가 포함되어 있으면 출력 없으면 없음 출력

--- src/main/java/christmas/OutputView.java
+++ src/main/java/christmas/OutputView.java
@@ -1,17 +1,21 @@
 package christmas;
 
 import christmas.EventCalculator.EventBadge;
+import christmas.EventCalculator.EventType;
 import christmas.FoodMenu.MenuList;
 import christmas.Messages.Main;
 import java.text.DecimalFormat;
+import java.util.List;
 import java.util.Map;
 
 public class OutputView {
     private static final String TOTAL_PURCHASE_AMOUNT="<할인 전 총주문 금액>";
-    private static final String MENU_AND_COUNT = "%s %d개";
     private static final String ORDER_MENU = "<주문 메뉴>";
+    private static final String PRESENT_MENU = "<증정 메뉴>";
+    private static final String MENU_AND_COUNT = "%s %d개";
     private static final String AMOUNT_MESSAGE="원";
     private static final String THREE_NUMBER_FORMAT="###,###";
+    private static final String NON_APPLIED = "없음";
     private static final DecimalFormat decFormat = new DecimalFormat(THREE_NUMBER_FORMAT);
     public void printWelcome(){
         System.out.println(Main.WELCOME_MESSAGE.getMessage());
@@ -29,7 +33,13 @@ public class OutputView {
             System.out.printf(MENU_AND_COUNT + "%n", ordered.getName(), purchaseMenus.get(ordered));
         }
     }
-    public void printPresentEvent(Customer customer){
+    public void printPresentEvent(List<EventType> appliedEvents){
+        System.out.println(PRESENT_MENU);
+        if (appliedEvents.contains(EventType.PRESENT)){
+            System.out.printf(MENU_AND_COUNT + "%n", MenuList.CHAMPAGNE.getName(), 1);
+            return;
+        }
+        System.out.println(NON_APPLIED);
 
     }
     public void printAppliedEvent(Customer customer){
