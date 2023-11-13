7ff672eae9e107c99e2e49e4b8e467c24d57bb93
i3kae <snoger1234@gmail.com>
2023-11-13 14:19:00 +0900

    feat: 방문 날짜, 구매 목록에 대한 getter 구현 / 적용 이벤트, 총혜택 금액 계산 메소드 구현 1. 방문 날짜(visitDate), 구매 목록(purchaseMenus)에 대한 getter 구현 2. eventCalculator의 calcEventList, calcTotalDiscount를 통한 적용 이벤트, 총헤택 금액 계산 메소드 구현

--- src/main/java/christmas/Customer.java
+++ src/main/java/christmas/Customer.java
@@ -1,10 +1,14 @@
 package christmas;
 
+import christmas.EventCalculator.EventType;
+import christmas.FoodMenu.MenuList;
+import java.util.List;
 import java.util.Map;
 
 public class Customer{
     private Integer visitDate;
     private Map<FoodMenu.MenuList, Integer> purchaseMenus;
+    private static final EventCalculator eventCalculator = new EventCalculator();
 
     public Customer(Integer visitDate, Map<FoodMenu.MenuList, Integer> purchaseMenus){
         this.visitDate = visitDate;
@@ -18,5 +22,16 @@ public class Customer{
         }
         return calcResult;
     }
-
+    public List<EventType> calcAppliedEvent(){
+        return eventCalculator.calcEventList(visitDate, calcPurchaseAmount());
+    }
+    public Integer calcTotalDiscountedAmount(){
+        return eventCalculator.calcTotalDiscount(visitDate, purchaseMenus, calcPurchaseAmount());
+    }
+    public Integer getVisitDate(){
+        return visitDate;
+    }
+    public Map<MenuList, Integer> getPurchaseMenus(){
+        return purchaseMenus;
+    }
 }
