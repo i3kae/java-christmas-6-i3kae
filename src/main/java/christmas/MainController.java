ccbe1f6d44627ac882d6c09a073896263703143e
i3kae <snoger1234@gmail.com>
2023-11-13 10:00:41 +0900

    feat: MainController에 InputView, OutputView, Customer 결합

--- src/main/java/christmas/MainController.java
+++ src/main/java/christmas/MainController.java
@@ -5,6 +5,9 @@ public class MainController {
     private final InputView inputView = new InputView();
 
     public void eventPlannerStart(){
+        Customer customer;
         outputView.printWelcome();
+        customer = new Customer(inputView.readDate(), inputView.readPurchaseMenu());
+
     }
 }
