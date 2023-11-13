c6353202ecbae57f78da0be45ae1fcb7736619cd
i3kae <snoger1234@gmail.com>
2023-11-13 15:44:12 +0900

    feat: 총혜택 금액, 할인 후 예상 결제 금액 출력 함수 구현

--- src/main/java/christmas/OutputView.java
+++ src/main/java/christmas/OutputView.java
@@ -12,11 +12,14 @@ public class OutputView {
     private static final String ORDER_MENU = "<주문 메뉴>";
     private static final String PRESENT_MENU = "<증정 메뉴>";
     private static final String APPLIED_EVENTS = "<혜택 내역>";
+    private static final String TOTAL_DISCOUNT_AMOUNT = "<총혜택 금액>";
+    private static final String ESTIMATED_AMOUNT = "<할인 후 예상 결제 금액>";
     private static final String MENU_AND_COUNT = "%s %d개";
     private static final String EVENT_AND_DISCOUNT = "%s: -";
     private static final String AMOUNT_MESSAGE="원";
     private static final String THREE_NUMBER_FORMAT="###,###";
     private static final String NON_APPLIED = "없음";
+    private static final String MINUS = "-";
     private static final DecimalFormat decFormat = new DecimalFormat(THREE_NUMBER_FORMAT);
     public void printWelcome(){
         System.out.println(Main.WELCOME_MESSAGE.getMessage());
@@ -51,10 +54,12 @@ public class OutputView {
         }
     }
     public void printTotalDiscount(Integer totalDiscount){
-
+        System.out.println(TOTAL_DISCOUNT_AMOUNT);
+        System.out.println(MINUS + decFormat.format(totalDiscount) + AMOUNT_MESSAGE);
     }
     public void printEstimatedAmount(Integer estimatedAmount){
-
+        System.out.println(ESTIMATED_AMOUNT);
+        System.out.println(decFormat.format(estimatedAmount) + AMOUNT_MESSAGE);
     }
     public void printEventBadge(EventBadge eventBadge){
 
