49d35a52e956bf642230835162df4091bba33636
i3kae <snoger1234@gmail.com>
2023-11-13 14:15:55 +0900

    feat: 이벤트 미리보기 안내 함수, 주문 메뉴 출력 함수 구현

--- src/main/java/christmas/OutputView.java
+++ src/main/java/christmas/OutputView.java
@@ -1,10 +1,15 @@
 package christmas;
 
+import christmas.EventCalculator.EventBadge;
+import christmas.FoodMenu.MenuList;
 import christmas.Messages.Main;
 import java.text.DecimalFormat;
+import java.util.Map;
 
 public class OutputView {
     private static final String TOTAL_PURCHASE_AMOUNT="<할인 전 총주문 금액>";
+    private static final String MENU_AND_COUNT = "%s %d개";
+    private static final String ORDER_MENU = "<주문 메뉴>";
     private static final String AMOUNT_MESSAGE="원";
     private static final String THREE_NUMBER_FORMAT="###,###";
     private static final DecimalFormat decFormat = new DecimalFormat(THREE_NUMBER_FORMAT);
@@ -14,5 +19,32 @@ public class OutputView {
     public void printTotalPurchaseAmount(Integer amount){
         System.out.println(TOTAL_PURCHASE_AMOUNT);
         System.out.println(decFormat.format(amount) + AMOUNT_MESSAGE);
+    }
+    public void printEventPreview(Integer visitDate){
+        System.out.printf(Main.EVENT_BENEFIT_PREVIEW.getMessage() + "%n", visitDate);
+    }
+    public void printOrderMenus(Map<MenuList, Integer> purchaseMenus){
+        System.out.println(ORDER_MENU);
+        for (MenuList ordered : purchaseMenus.keySet()){
+            System.out.printf(MENU_AND_COUNT + "%n", ordered.getName(), purchaseMenus.get(ordered));
+        }
+    }
+    public void printTotalAmount(Integer totalAmount){
+
+    }
+    public void printPresentEvent(Customer customer){
+
+    }
+    public void printAppliedEvent(Customer customer){
+
+    }
+    public void printTotalDiscount(Integer totalDiscount){
+
+    }
+    public void printEstimatedAmount(Integer estimatedAmount){
+
+    }
+    public void printEventBadge(EventBadge eventBadge){
+
     }
 }
