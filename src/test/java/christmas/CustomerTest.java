a8290b96e84f29378086ba83116a9d651048289a
i3kae <snoger1234@gmail.com>
2023-11-15 09:43:54 +0900

    test: 고객 클래스에 대한 테스트 추가 1. 총혜택 금액 테스트 2. 적용 이벤트 테스트 3. 이벤트 배지 테스트

--- src/test/java/christmas/CustomerTest.java
+++ src/test/java/christmas/CustomerTest.java
@@ -2,8 +2,11 @@ package christmas;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import christmas.EventCalculator.EventBadge;
+import christmas.EventCalculator.EventType;
 import christmas.FoodMenu.MenuList;
 import java.util.EnumMap;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -21,5 +24,25 @@ public class CustomerTest {
     @DisplayName("총구매 금액 계산 테스트 | 총구매 금액 테스트")
     void totalPurchaseAmountTest(){
         assertThat(customer.calcPurchaseAmount()).isEqualTo(268000);
+    }
+    @Test
+    @DisplayName("총구매 금액 계산 테스트 | 총혜택 금액 테스트")
+    void totalDiscountAmountTest(){
+        assertThat(customer.calcTotalDiscountedAmount()).isEqualTo(35469);
+    }
+    @Test
+    @DisplayName("총구매 금액 계산 테스트 | 적용 이벤트 테스트")
+    void appliedEventTest(){
+        Map<EventType, Integer> appliedEvents = new EnumMap<>(EventType.class);
+        appliedEvents.put(EventType.WEEKDAY, 6069);
+        appliedEvents.put(EventType.CHRISTMAS, 3400);
+        appliedEvents.put(EventType.SPECIAL, 1000);
+        appliedEvents.put(EventType.PRESENT, 25000);
+        assertThat(customer.calcAppliedEvent()).isEqualTo(appliedEvents);
+    }
+    @Test
+    @DisplayName("총구매 금액 계산 테스트 | 이벤트 배지 테스트")
+    void eventBadgeTest(){
+        assertThat(customer.calcEventBadge()).isEqualTo(EventBadge.SANTA);
     }
 }
